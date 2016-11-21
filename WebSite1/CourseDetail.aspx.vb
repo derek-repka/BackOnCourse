c74ef4462b23248c0dabcba49449d424d05bc315
Eric <ediep@ualberta.ca>
2016-11-21 12:33:05 -0700

    Product Page

--- WebSite1/CourseDetail.aspx.vb
+++ WebSite1/CourseDetail.aspx.vb
@@ -1,5 +1,8 @@
 ﻿
-Partial Class Course_Detail
+Partial Class CourseDetail
     Inherits System.Web.UI.Page
 
+    Protected Sub Page_Init(sender As Object, e As EventArgs)
+        Dim id As Integer = Request.QueryString("id")
+    End Sub
 End Class
