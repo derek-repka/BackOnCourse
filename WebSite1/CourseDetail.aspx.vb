87aaee68bd1afd37ff38a48f2ee9813d52e8671a
Eric <Eric>
2016-11-23 12:24:36 -0700

    Added Workshop page

--- WebSite1/CourseDetail.aspx.vb
+++ WebSite1/CourseDetail.aspx.vb
@@ -5,4 +5,7 @@ Partial Class CourseDetail
     Protected Sub Page_Init(sender As Object, e As EventArgs)
         Dim id As Integer = Request.QueryString("id")
     End Sub
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
 End Class
