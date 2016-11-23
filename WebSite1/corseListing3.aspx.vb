1b7dd33308606ee32d54b7fbb8b0b7ab8f32d3fe
buslabs <buslabs@B18-W04>
2016-11-23 12:39:33 -0700

    Changed some aspects of the search page

--- WebSite1/corseListing3.aspx.vb
+++ WebSite1/corseListing3.aspx.vb
@@ -2,8 +2,28 @@
 Partial Class corseListing3
     Inherits System.Web.UI.Page
 
+    Private Sub calanderButton_Click(sender As Object, e As EventArgs) Handles calanderButton.Click
+        If (Calendar1.Visible = False) Then
+            Calendar1.Visible = True
+        Else
+            Calendar1.Visible = False
+        End If
+    End Sub
+
+    Private Sub Calendar1_SelectionChanged(sender As Object, e As EventArgs) Handles Calendar1.SelectionChanged
+        courseDate.Text = Calendar1.SelectedDate.ToShortDateString()
+        Calendar1.Visible = False
+    End Sub
+
+    Private Sub Courses_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Courses.SelectedIndexChanged
+        Response.Redirect("CourseDetail")
+    End Sub
+
+
+
     Private Sub searchButton_Click(sender As Object, e As EventArgs) Handles searchButton.Click
         Courses.Visible = True
-        Courses.Enabled = True
     End Sub
+
+
 End Class
