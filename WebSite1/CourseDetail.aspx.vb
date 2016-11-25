39efef3c94aff5fe104e0ab73e5363022c4d7e5d
buslabs <buslabs@B24-W31>
2016-11-25 16:12:00 -0700

    CourseDetail now adds to sqlServer

--- WebSite1/CourseDetail.aspx.vb
+++ WebSite1/CourseDetail.aspx.vb
@@ -11,12 +11,43 @@ Partial Class CourseDetail
         If dv.Count > 0 Then
             TopicHolder.Text = dv(0)(8)
             Firm.Text = dv(0)(2)
+            Instructor.Text = dv(0)(4) + " " + dv(0)(3)
             Description.Text = dv(0)(11)
             Status.Text = dv(0)(12)
+            If Status.Text = "N" Then
+                Status.Text = "No"
+                CartCourseButton.Text = "Not available"
+                CartCourseButton.Enabled = False
+
+            ElseIf Status.Text = "Y" Then
+                Status.Text = "Yes"
+            End If
+
             Price.Text = dv(0)(10)
-        End If
+            End If
     End Sub
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
 
+    End Sub
+    Protected Sub CartCourse_Click(sender As Object, e As EventArgs) Handles CartCourseButton.Click
+        Dim productid As Integer = Request.QueryString("id")
+        Dim userid As Integer = Session("id")
+        Dim userDV As DataView = CType(userSqlDataSource.Select(DataSourceSelectArguments.Empty), DataView)
+
+        If userDV.Count = 0 Then
+            Dim curDate As String = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+            userSqlDataSource.InsertParameters("orderDate").DefaultValue = curDate
+            userSqlDataSource.InsertParameters("userID").DefaultValue = userid
+            userSqlDataSource.InsertParameters("orderStatusId").DefaultValue = 1
+            userSqlDataSource.Insert()
+
+        End If
+        userDV = CType(userSqlDataSource.Select(DataSourceSelectArguments.Empty), DataView)
+        productSqlDataSource.InsertParameters("orderID").DefaultValue = userDV(0)(0)
+        productSqlDataSource.InsertParameters("productID").DefaultValue = productid
+        productSqlDataSource.InsertParameters("workshopID").DefaultValue = Nothing
+        productSqlDataSource.InsertParameters("quantity").DefaultValue = 1
+
+        Dim count As Integer = productSqlDataSource.Insert()
     End Sub
 End Class
